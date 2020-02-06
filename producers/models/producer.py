88aa2f026d0253ed83698d04798ba6301b36ef19
Sage Rimal <srimal@campusexplorer.com>
2020-02-05 16:30:47 -0800

    Update producer.py

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -2,8 +2,6 @@
 import logging
 import time
 
-
-from confluent_kafka import avro
 from confluent_kafka.admin import AdminClient, NewTopic
 from confluent_kafka.avro import AvroProducer
 
@@ -15,16 +13,14 @@ class Producer:
 
     # Tracks existing topics across all Producer instances
     existing_topics = set([])
-    BROKER_URL = "PLAINTEXT://localhost:9092"
-    SCHEMA_REGISTRY_URL = "http://localhost:8081"
 
     def __init__(
-        self,
-        topic_name,
-        key_schema,
-        value_schema=None,
-        num_partitions=1,
-        num_replicas=1,
+            self,
+            topic_name,
+            key_schema,
+            value_schema=None,
+            num_partitions=1,
+            num_replicas=1
     ):
         """Initializes a Producer object with basic settings"""
         self.topic_name = topic_name
@@ -32,14 +28,9 @@ class Producer:
         self.value_schema = value_schema
         self.num_partitions = num_partitions
         self.num_replicas = num_replicas
-
-
-        # TODO: Configure the broker properties below. Make sure to reference the project README
-        # and use the Host URL for Kafka and Schema Registry!
-    
         self.broker_properties = {
-            "bootstrap.servers": BROKER_URL,
-            "schema_registry" = SCHEMA_REGISTRY_URL 
+            'bootstrap.servers': 'PLAINTEXT://0.0.0.0:9092,PLAINTEXT://0.0.0.0:9093,PLAINTEXT://0.0.0.0:9094',
+            'schema.registry.url': 'http://0.0.0.0:8081'
         }
 
         # If the topic does not already exist, try to create it
@@ -47,32 +38,41 @@ class Producer:
             self.create_topic()
             Producer.existing_topics.add(self.topic_name)
 
-        # TODO: Configure the AvroProducer
-        self.producer = AvroProducer({"bootstrap.servers": BROKER_URL},
-        schema_registry=SCHEMA_REGISTRY_URL)
+        self.producer = AvroProducer(
+            config=self.broker_properties,
+            default_key_schema=self.key_schema,
+            default_value_schema=self.value_schema)
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
-        #
-        #
-        # TODO: Write code that creates the topic for this producer if it does not already exist on
-        # the Kafka Broker.
-        #
-        #
-        logger.info("topic creation kafka integration incomplete - skipping")
 
-    def time_millis(self):
-        return int(round(time.time() * 1000))
+        logger.debug("producer topic creation function working")
+        client = AdminClient({'bootstrap.servers': self.broker_properties.get('bootstrap.servers')})
+        print(f'MY TOPICS ARE: {client.list_topics().topics}')
+        topic_dict = client.list_topics().topics
+        if self.topic_name not in topic_dict:
+  
+            futures = client.create_topics([NewTopic(topic=self.topic_name,
+                                                     num_partitions=self.num_partitions,
+                                                     replication_factor=self.num_replicas)])
+
+            for topic, future in futures.items():
+                try:
+                    future.result()
+                    logger.info(f"Topic {topic} created")
+                except Exception as e:
+                    logger.fatal(f"Failed to create topic {topic}: {e}")
+        else:
+            logger.info(f"Topic {self.topic_name} already exists")
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
-        #
-        #
-        # TODO: Write cleanup code for the Producer here
-        #
-        #
-        logger.info("producer close incomplete - skipping")
+        logger.debug("producer close function working")
+        if self.producer is not None:
+            logger.debug("Flushing producer")
+            self.producer.flush()
 
-    def time_millis(self):
+
+    def time_millis():
         """Use this function to get the key for Kafka Events"""
         return int(round(time.time() * 1000))
